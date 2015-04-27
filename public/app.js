6921612a80c860d1dcf00a8c35edc7159083371d
unknown <matteo@PROMETEO.mantricsdv.com>
2015-04-27 11:22:40 +0200

    app state using route

--- public/app.js
+++ public/app.js
@@ -21,28 +21,39 @@ module.exports = function(app) {
     }
   ];
 
+
+  me.route = function(mediaType) {
+    if (!mediaType)
+      return viewTypes();
+    var type = types.filter(function(type) { return type.name == mediaType; })[0];
+    if (type)
+      viewMedia(type.filter, mediaType);
+  }
+
+
+
   function viewTypes() {
     $media.html(types.map(function(type) {
       return $(me.tmpl.type(type)).data(type);
     }))
   }
 
-  function viewMedia(filters) {
+  function viewMedia(filters, type) {
     $media.html('loading media');
     me.api.get('.json', { filter: JSON.stringify(filters) }, function(response) {
-      me.$root.find('.media').html(response.map(function(item) {
+      me.$root.find('.media').html(['<a href="' + app.url + '"><div class="mashmc-list-item"><h2>.. back</h2></div></a>'].concat(response.map(function(item) {
         return $(me.tmpl.media(item)).data(item);
-      }));
+      })));
     });
   }
 
-  viewTypes();
+  
 
   $media
-    .on('click', '.media-type', function() {
+    /*.on('click', '.media-type', function() {
       var type = $(this).data();
       viewMedia(type.filter);
-    })
+    })*/
     .on('click', '.mashmc-list-item', function() {
       console.log($(this).data());
     });
